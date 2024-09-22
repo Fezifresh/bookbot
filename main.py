4663ad9cc9857f7ee28b870ac2d15934e0ac2fac
Fezifresh <felix.fassig@googlemail.com>
2024-09-22 16:28:22 +0200

    report counts words and letters

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 def main():
     book_path = 'books/frankenstein.txt'
+    number_of_characters = count_characters(book_path)
+    number_of_words = wordcount(book_path)
+    text = book_text(book_path)
+
+    #Report#
+
+    print(f"This is the report for {book_path}\n")
+
+    print(f"{number_of_words} words found in the document\n")
+    
+    print("This is a list of the characters and the number of their appereance:")
+    for a in number_of_characters:
+        print(f"The character {a} appeares {number_of_characters[a]} times")
+    
+    print("\nend of report")
+
+def count_characters(book_path):
+    text = book_text(book_path).lower()
+    dict_of_characters = {}
+
+    #Liste aller Kleinbuchstaben#
+    import string
+    string.ascii_lowercase
+    letters = (string.ascii_lowercase)
+    ##
+
+    for character in text:
+        if character not in dict_of_characters and character in letters:
+            dict_of_characters[character] = 1
+        elif character in letters:
+            dict_of_characters[character] += 1
+    return dict_of_characters
+
+
+def wordcount(book_path):    
     words = book_text(book_path).split()
     wordcount = len(words)
-    print(f"{wordcount} words found in document")
+    return wordcount
 
 def book_text(book_path):
-    text = get_book_text(book_path)
-    return(text)
-
-def get_book_text(path):
-    book_text = open(path)
-    return book_text.read()
+    book_text = open(book_path)
+    text_string = book_text.read()
+    return text_string
 
 main()
-
-
-# Meine Lösung
-# text = open('books/frankenstein.txt','r')
-# print(text.read())
-# text.close()
